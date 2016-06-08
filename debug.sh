7f1aecbd8872927c0db7e0aafd4987732ca957ec
Andrew Heebum Kwak <andrew@norux.me>
2016-06-08 16:06:57 +0900

    debug.sh 스크립트 안정화 작업

--- debug.sh
+++ debug.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-node_modules/electron-prebuilt/cli.js .
+ELECTRON_BIN=node_modules/electron-prebuilt/cli.js
+WORKING_DIR=`dirname $0`
+
+if [ -f $ELECTRON_BIN ] ; then
+    $ELECTRON_BIN $WORKING_DIR
+else
+    echo "You need npm install first."
+fi
