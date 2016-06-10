134ec6528bab9c86c3b9b4debf5b42c5ca830625
sangjun.lee <bluevia@hu-nam-nam-ui-iMac.local>
2016-06-10 18:19:22 +0900

    스타일 수정 / Gulpfile.js 수정

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function(){
-    gulp.src('css/style.sass')
+    gulp.src('app/css/style.sass')
         .pipe(sass().on('error',sass.logError))
-        .pipe(gulp.dest('./css/dist'));
+        .pipe(gulp.dest('./app/css/dist'));
 })
 
 //watch task
 gulp.task('default', function(){
-    gulp.watch('css/**/*.sass',['styles'])
+    gulp.watch('app/css/**/*.sass',['styles'])
 })
 
